2f09d0d79d50ec55ff38e54d9b12f5474c6c27c2
mingeydacoder <97391087+mingeydacoder@users.noreply.github.com>
2022-04-27 16:42:41 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,8 +1,3 @@
-const para = document.querySelector('li.special');
-
-para.addEventListener('click', updateName);
-
-function updateName() {
-  let name = prompt('更改文字');
-  para.textContent = name;
+function cool(){
+	document.getElementById("ex").innerHTML = "JavaScript"
 }
