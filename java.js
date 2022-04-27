fb80f7e9a49efe0d85610cecb721cda8ecb75f46
mingeydacoder <97391087+mingeydacoder@users.noreply.github.com>
2022-04-27 22:34:07 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,3 +1,16 @@
-function cool(){
-	document.getElementById("ex").innerHTML = "JavaScript"
+function cool() {
+  document.getElementById("ex").innerHTML = "JavaScript"
+}
+
+function password(pwd) {
+
+  if (pwd == "0627") {
+    var str = "Hello world";
+    var URL = "http://www.yiibai.com";
+    alert(str.link(URL));
+  } else
+
+  {
+    alert("Denied !");
+  }
 }
